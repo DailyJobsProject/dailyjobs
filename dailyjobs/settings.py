729f5f5a941d9730ac108102cd688b218dd4969a
raicubogdan <raicubogdan15@gmail.com>
2020-07-24 18:22:35 +0300

    fixed user default profile pic

--- dailyjobs/settings.py
+++ dailyjobs/settings.py
@@ -120,18 +120,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
-
-LOGIN_REDIRECT_URL = '/'
-
-LOGOUT_REDIRECT_URL = '/'
-
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static')
-    ]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = (
+     os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- users/models.py
+++ users/models.py
@@ -13,12 +13,12 @@ class Company(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=254)
     telephone = models.CharField(max_length=254)
-    about = models.TextField(max_length=1000)
+    about = models.TextField(max_length=1000, default='Tell us something about you')
     image = models.ImageField(upload_to='profile_pics')
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         return reverse_lazy('users:company_detail', kwargs={'pk': self.pk})
 
@@ -27,16 +27,16 @@ class Company(models.Model):
         if self.image and hasattr(self.image, 'url'):
             return self.image.url
         else:
-            return 'media/profile_pics/user.jpg'
+            return 'this'
 
 class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     telephone = models.CharField(max_length=254)
-    about = models.TextField(max_length=1000)
-    image = models.ImageField(upload_to='profile_pics', default='')
-    
+    about = models.TextField(max_length=1000, default='Tell us something about you')
+    image = models.ImageField(upload_to='profile_pics')
+
 
 
     def __str__(self):
@@ -50,7 +50,7 @@ class Employee(models.Model):
         if self.image and hasattr(self.image, 'url'):
             return self.image.url
         else:
-            return 'media/profile_pics/user.jpg'
+            return 'media/profile_pics/user.png'
 
 class Document(models.Model):
     document = models.FileField(upload_to='media/', max_length=255, blank=True)
