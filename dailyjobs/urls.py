d5c2c993579a38deec16ba07f518b70a4b637d7f
zulu607i <andreimmircea1@gmail.com>
2020-08-10 17:56:24 +0300

    added background to homepage

--- dailyjobs/urls.py
+++ dailyjobs/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import  staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +28,4 @@ urlpatterns = [
 
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
